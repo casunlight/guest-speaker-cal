b00c5860c8d445275f44f70a5ac21c093f6afdad
jongrover <hello@jonathangrover.com>
2013-09-18 16:49:36 -0400

    finish collapse logo fixed header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,36 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function() {
+//collapsing navigation on down scroll to specified change point.
+
+    var changePoint = 100, //y-axis scroll point at which to alter the navigation
+        currentPoint = 0; //initially set current point to zero.
+
+    function minifyNav() {
+      $('object#minify').css({'display':'block'});
+      $('object#bigify').css({'display':'none'});
+      $('header').css({'height':'65px'});
+      $('nav').css({'margin-top':'10px'});
+      $('object#minify').css({'margin':'10px 0 0'});
+    }
+
+    function bigifyNav() {
+      $('object#minify').css({'display':'none'});
+      $('object#bigify').css({'display':'block'});
+      $('header').css({'height':'auto'});
+      $('nav').css({'margin':'35px 0 0'});
+    }
+
+    $(window).scroll(function() { //windows scrolls event.
+      currentPoint = $(document).scrollTop(); //update the current scroll position.
+      //console.log(currentPoint);
+      if (currentPoint > changePoint) {
+        minifyNav(); //shrink nav
+      } else {
+        bigifyNav(); ///bigify nav
+      }
+    });
+
+});
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :get_week_dates
+
+  def get_week_dates
+    @week_dates = []
+    weeks = Week.order("id ASC")
+    weeks.each do |week|
+      @week_dates << week.days.order("id ASC").first.date
+      @week_dates << week.days.order("id ASC").last.date
+    end
+  end
 end
