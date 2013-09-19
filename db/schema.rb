33e6183a505cc5efb9bd90c83ad91400deac4c1d
aviflombaum <avi@flombaum.com>
2013-09-19 17:12:39 -0400

    database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130917144520) do
+ActiveRecord::Schema.define(:version => 20130916183355) do
 
   create_table "days", :force => true do |t|
     t.date     "date"
